9b3776758cd1c8ab32bb62822a18464e0203103d
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-08-09 09:47:37 -0700

    Shift route order to close contacts page

--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -1,52 +1,41 @@
 const router = require('express').Router()
 const user = require('../../db/users')
-
 const { hashPassword, comparePassword } = require('../utils')
 
-
 router.get('/signup', (request, response) => {
-    console.log('GET REQUEST')
-    response.render('signup')
+  response.render('signup')
 })
 
 router.post('/signup', (request, response) => {
-    const { username, password } = request.body
-     if (username.length === 0 || password.length === 0) {
-         response.render('signup', { error: "Username or Password CANNOT be blank" })   
-     } 
-    user.create(username, hashPassword(password, 10))
-    .then(user => {
-            request.session.user = user;
-            response.redirect('/')
-    })
-    .catch(error => {
-        console.log(error.message)
-        response.render('signup', { error }) 
-    })
-    // .then(user => console.log(`User:: ${JSON.stringify(user)}`))
+  const { username, password } = request.body
+   if (username.length === 0 || password.length === 0) {
+       response.render('signup', { error: "Username or Password CANNOT be blank" })
+   }
+  user.create(username, hashPassword(password, 10))
+  .then(user => {
+          request.session.user = user;
+          response.redirect('/')
+  })
+  .catch(error => {
+      console.log(error.message)
+      response.render('signup', { error })
+  })
 })
 
 router.get('/login',  (request, response) => {
-    response.render('login')
+  response.render('login')
 })
 
 router.post('/login', (request, response) => {
-    const {username, password} = request.body
-    user.findUser(username)
+  const {username, password} = request.body
+  user.findUser(username)
     .then(user => {
-        if (!user || !(comparePassword(password, user.password))) {
-            response.render('login', {error: "Username or Password is invalid" })
-        } else {
-            response.redirect('/')
-        }
-    }
+      if (!user || !(comparePassword(password, user.password))) {
+        response.render('login', {error: "Username or Password is invalid" })
+      } else {
+        response.redirect('/')
+      }
+  }
+)})
 
-    )})
-
-
-// salt password -- signup
-// compared salted password to stored hashed password
-//session checker middleware for ALL routes
-// login uncessessful redirect and message
 module.exports = router
-
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -4,13 +4,15 @@ const DbContacts = require('../../db/contacts');
 const auth = require('./auth')
 const { isLoggedIn } = require('../utils')
 
+router.use(auth);
+router.use(isLoggedIn)
+
 router.get('/', (request, response) => {
   DbContacts.getContacts()
     .then((contacts) => {response.render('index', { contacts })})
     .catch( err => console.log('err', err) )
 })
-router.use(auth);
-router.use(isLoggedIn)
+
 router.use('/contacts', contacts); // /contacts/search
 
 module.exports = router;
